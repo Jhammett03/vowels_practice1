762cf36f21fac550f2c7a4c4b4914e3474a40d4e
Jhammett03 <116196579+Jhammett03@users.noreply.github.com>
2023-02-03 17:31:44 -0800

    Moving files

---
+++ test_cases.py
@@ -0,0 +1,11 @@
+import vowels
+import unittest
+
+class TestCases(unittest.TestCase):
+
+    def vowels_test(self):
+        input = "hello"
+        result = vowels.py.vowel_count(input)
+        expected = 2
+        self.assertEqual(expected,result)
+
---
+++ vowels.py
@@ -0,0 +1,14 @@
+# Purpose: Given a word, count the number of vowels in the word
+# Input Type: str
+# Output Type: int
+# Example Input: "hello"
+# Output of given input: 2
+class vowels():
+    def vowel_count(word: str) -> int:
+        count = 0
+        vowels = ["a","e","i","o","u"]
+        for letters in word:
+            if letters.lower() in vowels:
+                count += 1
+        print(count)
+        return count
